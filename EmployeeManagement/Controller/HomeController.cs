39caed82d5a0f9a18e4617393de4e0fe1bc91cd1
Shabih <Shabih.sharfani@gmail.com>
2020-07-07 19:09:44 -0500

    chore():ViewImport , ViewStart and basic DTO for employee
    Added the ViewImport , ViewStart and basic DTO for employee Project

--- EmployeeManagement/Controller/HomeController.cs
+++ EmployeeManagement/Controller/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using EmployeeManagement.Models;
+﻿using EmployeeManagement.DTO_s;
+using EmployeeManagement.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,24 +8,59 @@ using System.Threading.Tasks;
 
 namespace EmployeeManagement.Controllers
 {
-    public class HomeController : Controller
+public class HomeController : Controller
+{
+    private readonly IEmployeeRepository _employeeRepository;
+
+    // This is called Constructor Injection .Injecting the repository inside the Home Controller constructor.
+    public HomeController(IEmployeeRepository employeeRepository)
     {
-        private readonly IEmployeeRepository _employeeRepository;
-
-        // This is called Constructor Injection .Injecting the repository inside the Home Controller constructor.
-        public HomeController(IEmployeeRepository employeeRepository)
-        {
-            _employeeRepository = employeeRepository;
-        }
-        public Employee Index()
-        {
-           return _employeeRepository.GetEmployee(2);
-        }
-
-        public ViewResult Details()
-        {
-            Employee employee = _employeeRepository.GetEmployee(1);
-            return View("~/MyViews/Test.cshtml");
-        }
+        _employeeRepository = employeeRepository;
     }
+
+        [Route("")]
+        [Route("Home")]
+        [Route("Home/Index")]
+        public ViewResult Index()
+    {
+        var model =  _employeeRepository.GetAllEmployees();
+        return View("~/Views/Home/Index.cshtml",model);
+
+    }
+        [Route("Home/Details/{id?}")]
+        public ViewResult Details(int? id)
+    {
+        //Employee employee = _employeeRepository.GetEmployee(id);
+
+        // Use of View Data for passing values from Controller to View.
+        // ViewData["Employee"] = employee;
+        // ViewData["PageTitle"] = "Employee Details";
+
+        // Use of View Bag
+        //ViewBag.Employee = employee;
+        //ViewBag.PageTitile = "Employee Details";
+
+
+
+        //return View("~/MyViews/Test.cshtml");
+
+        // Relative path for views
+        // return View("../../MyViews/Test");
+
+        // Passing the values using strongly typed view.
+        //ViewBag.PageTitle = "Employee Details";
+        //return View(employee);
+
+
+        // Use of DTO now.
+
+        HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
+        {
+            Employee = _employeeRepository.GetEmployee(id??1),
+            PageTitle = "Employee Details Homie"
+        };
+
+        return View(homeDetailsViewModel);
+    }
+}
 }
---
+++ EmployeeManagement/DTO's/HomeDetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.DTO_s
+{
+    public class HomeDetailsViewModel
+    {
+        public Employee Employee { get; set; }
+        public string PageTitle { get; set; }
+    }
+}
