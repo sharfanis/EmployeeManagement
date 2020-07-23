75cc0a7e0a173f16d2e4ce8aac6915d38cca6c96
Shabih <Shabih.sharfani@gmail.com>
2020-07-22 21:14:10 -0500

    WIP(): IMage Upload Logic
    Lots of details and .Net Core Libs imported for Image upload and to be saved in DB

--- EmployeeManagement/Controller/HomeController.cs
+++ EmployeeManagement/Controller/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using EmployeeManagement.DTO_s;
 using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,13 +14,18 @@ namespace EmployeeManagement.Controllers
     [Route("[controller]/[action]")]
 public class HomeController : Controller
 {
-    private readonly IEmployeeRepository _employeeRepository;
+       private readonly IEmployeeRepository _employeeRepository;
+        private readonly Microsoft.AspNetCore.Hosting.IHostingEnvironment hostingEnvironment;
 
-    // This is called Constructor Injection .Injecting the repository inside the Home Controller constructor.
-    public HomeController(IEmployeeRepository employeeRepository)
+        //private readonly IHostingEnvironment hostingEnvironment;
+
+        // This is called Constructor Injection .Injecting the repository inside the Home Controller constructor.
+        public HomeController(IEmployeeRepository employeeRepository , Microsoft.AspNetCore.Hosting.IHostingEnvironment hostingEnvironment)
     {
         _employeeRepository = employeeRepository;
-    }
+   
+            this.hostingEnvironment = hostingEnvironment;
+        }
 
         // Added Routes for Routing through Attribute
         [Route("")]
@@ -37,17 +45,53 @@ public class HomeController : Controller
         }
 
         [HttpPost]
-        public IActionResult Create(Employee employee) 
+        //public IActionResult Create(Employee employee) 
+        //{
+        //// IAction Result is parent of ViewResult and RedirectToActionResult.
+
+        //    if (ModelState.IsValid) // This for checking if the validation on field is present.
+        //    {
+        //        Employee newEmployee = _employeeRepository.AddEmployee(employee);
+        //        return RedirectToAction("details", new { id = newEmployee.Id });
+        //    }
+
+        //    return View();
+
+
+        //}
+
+        public IActionResult Create(EmployeeCreateViewModel model)
         {
-        // IAction Result is parent of ViewResult and RedirectToActionResult.
+            // IAction Result is parent of ViewResult and RedirectToActionResult.
 
             if (ModelState.IsValid) // This for checking if the validation on field is present.
             {
-                Employee newEmployee = _employeeRepository.AddEmployee(employee);
+                string uniqueFileName = null;
+
+                if(model.Photo != null)
+                {
+                    string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                    model.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
+                }
+
+                Employee newEmployee = new Employee
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    Department = model.Department,
+                    PhotoPath = uniqueFileName
+                };
+
+                _employeeRepository.AddEmployee(newEmployee);
                 return RedirectToAction("details", new { id = newEmployee.Id });
+
             }
 
             return View();
+
+
         }
 
 
