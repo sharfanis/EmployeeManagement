9b1667a951b36554f36c498e41e670987c24dbea
Shabih <Shabih.sharfani@gmail.com>
2020-07-29 15:29:09 -0500

    WIP(): Edit functionality of employees
    edit pictures only if they are changed , exposing filestream using IO reader

---
+++ EmployeeManagement/DTO's/EmployeeEditViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.DTO_s
+{
+    public class EmployeeEditViewModel : EmployeeCreateViewModel
+    {
+        public int Id { get; set; }
+
+        public string ExistingPhotoPath { get; set; }
+    }
+}
