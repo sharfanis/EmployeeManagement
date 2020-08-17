1a23e6d306475832af97b09949ed99b05d53bd65
Shabih <Shabih.sharfani@gmail.com>
2020-08-16 19:19:32 -0500

    WIP(): Importing IdentityDbContext
    This commit has some errors related to the importing of IdentityDbContext.  Next commit will fix this

--- EmployeeManagement/Models/AppDbContext.cs
+++ EmployeeManagement/Models/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,47 +7,45 @@ using System.Threading.Tasks;
 
 namespace EmployeeManagement.Models
 {
-    public class AppDbContext : DbContext
-    {
-        public AppDbContext(DbContextOptions<AppDbContext> options) 
-            : base(options)
-        {
+    public class AppDbContext : IdentityDbContext
 
-        }
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        { }
 
         public DbSet<Employee> Employees { get; set; }
 
         // By this we can seed our employee table with dummy data.
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
+        //protected override void OnModelCreating(ModelBuilder builder)
+        //{
 
-            // Either you can put stuff here or Make new ModelBuilder Extenstion class.
+        //    // Either you can put stuff here or Make new ModelBuilder Extenstion class.
 
-            //modelBuilder.Entity<Employee>().HasData(
-            //    new Employee
-            //    {
-            //        Id = 1,
-            //        Name = "Subuhi Sharfani",
-            //        Department = Dept.Medico,
-            //        Email = "Subuhi.sharfani@gmail.com"
-            //    },
-            //    new Employee {
-            //        Id = 2,
-            //        Name = "Natalia Sharfani",
-            //        Department = Dept.HR,
-            //        Email = "Natalia.sharfani@gmail.com"
-            //    }
+        //    //modelBuilder.Entity<Employee>().HasData(
+        //    //    new Employee
+        //    //    {
+        //    //        Id = 1,
+        //    //        Name = "Subuhi Sharfani",
+        //    //        Department = Dept.Medico,
+        //    //        Email = "Subuhi.sharfani@gmail.com"
+        //    //    },
+        //    //    new Employee {
+        //    //        Id = 2,
+        //    //        Name = "Natalia Sharfani",
+        //    //        Department = Dept.HR,
+        //    //        Email = "Natalia.sharfani@gmail.com"
+        //    //    }
 
-            //    );
-
-
-
-            // Since we have put stuff in ModelBuilderExtenstion we can use the Seed Method.
-            modelBuilder.Seed();
+        //    //    );
 
 
 
-        }
+        //    // Since we have put stuff in ModelBuilderExtenstion we can use the Seed Method.
+        //     base.OnModelCreating(builder);
+        //     builder.Seed();
+
+        //}
 
 
 
